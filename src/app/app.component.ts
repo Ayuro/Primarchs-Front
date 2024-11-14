8a3a1fe4e585e107c6573c39d755337a05f50b32
Ayuro <ayuroko@gmail.com>
2024-11-14 17:41:33 +0100

    feat: basics implementations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  message: string = '';
+  constructor(private apiService: ApiService) {}
+  ngOnInit() {
+    this.apiService.getMessage().subscribe(
+      (data: any) => {
+        this.message = data;
+      },
+      (error: any) => {
+        console.error('An error occurred:', error);
+      }
+    );
+  }
   title = 'Primachs-Front';
 }
